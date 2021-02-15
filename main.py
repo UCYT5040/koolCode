7fe972e4576cee42b64488b21c86e69abb71fd91
UCYT5040 <46662314+UCYT5040@users.noreply.github.com>
2021-02-14 18:02:02 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,72 @@
+import json
+modulesInfo = {}
+modules = []
+def runString(code):
+    global modules
+    quoteOpen = False
+    specialCharOpen = False
+    buildingKeyword = False
+    keywordBuild = ""
+    quoteBuild = ""
+    keywords = []
+    quotes = []
+    for i in code:
+        char = i
+        if char == "\"":
+            if quoteOpen:
+                quoteOpen = False
+                quotes.append(quoteBuild)
+                quoteBuild = ""
+            else:
+                quoteOpen = True
+        elif quoteOpen:
+            quoteBuild = quoteBuild + char
+        elif not " " == char:
+            keywordBuild = keywordBuild + char
+        elif " " == char:
+            keywords.append(keywordBuild)
+            keywordBuild = ""
+    for i in keywords:
+        keyword = i.lower()
+        if keyword == "print":
+            for i1 in quotes:
+                print(i1)
+        elif keyword == "add":
+            for i1 in quotes:
+                quote = i1.lower()
+                if quote == "python":
+                    modulesInfo["python"] = {"runLang":"python","runpy":"%quote%"}
+                    modules.append("python")
+                else:
+                    print("koolCode WARN: custom modules not supported/module not found.")
+        else:
+            for i1 in modules:
+                print(str(i1))
+                print(modulesInfo["python"])
+                print(modulesInfo[i1])
+                if modulesInfo[i1]["runLang"] == "python":
+                    try:
+                        for i2 in quotes:
+                            quote = i2
+                            code = modulesInfo[str(i1)][keyword].replace("%quote%", quote)
+                            eval(code)
+                    except:
+                        print(f"koolCode ERROR: Unknown keyword or module fail.")
+def runFile(dir):
+    try:
+        file = open(dir)
+        content = file.readlines()
+        content = [x.strip() for x in content]
+        for i in content:
+            runString(i) 
+    except:
+        print(f"koolCode ERROR: File not found ({dir})")
+def noFile():
+    runString(input("koolCode>"))
+option = input("Welcome to koolCode console.\nWould you like to open a file? y/n\n")
+if not option.lower() == "y":
+    print("A file will not be opened. Type any commands below.")
+    while True:
+        noFile()
+else:
+    runFile(input("Our offcial file extention is .koc however you can open any file. Please type in the directory of the file. See Open Files in our guide for more info.\nFILE DIRECTORY:"))
